1db2e936e2c4c2a81efa3583163771af2b99c4ce
shirosaki-hana <usagi@kirimasyaro.net>
2026-01-16 15:31:47 +0900

    레거시 더미 스크린 삭제

--- src/renderer/src/components/translation/index.ts
+++ src/renderer/src/components/translation/index.ts
@@ -1,5 +1,4 @@
 export { default as SessionSidebar, SIDEBAR_WIDTH } from './SessionSidebar';
-export { default as TranslationEditor } from './TranslationEditor';
 export { default as ChunkProgressPanel } from './ChunkProgressPanel';
 export { default as ControlPanel } from './ControlPanel';
 export { default as SessionDialog } from './SessionDialog';
